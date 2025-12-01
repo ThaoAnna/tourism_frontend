def48b757f9e6a560c2e698dbc4d50d0a30ebcc2
Thao Anna <annaphuongthao147@gmail.com>
2025-12-01 21:18:20 +0100

    change booking form details

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { AuroraCanvas } from "../components/ui/aurora-canvas";
 // ---------------------------
 // Helper functions (no imports)
 // ---------------------------
